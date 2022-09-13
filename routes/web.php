efa6903a5662813eb4833e844ac24ba03475c70e
waras47 <saudinwahyudin@gmail.com>
2022-09-13 09:52:16 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', 'HomepageController@index');
 Route::get('/about', 'HomepageController@about');
 Route::get('/kontak', 'HomepageController@kontak');
@@ -30,25 +26,27 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::get('/', 'DashboardController@index');
   // route kategori
   Route::resource('kategori', 'KategoriController');
-
+  // route produk
   Route::resource('produk', 'ProdukController');
-
+  // route customer
   Route::resource('customer', 'CustomerController');
-
+  // route transaksi
   Route::resource('transaksi', 'TransaksiController');
-
+  // profil
   Route::get('profil', 'UserController@index');
   // setting profil
   Route::get('setting', 'UserController@setting');
-
   // form laporan
   Route::get('laporan', 'LaporanController@index');
   // proses laporan
   Route::get('proseslaporan', 'LaporanController@proses');
+  // image
+  Route::get('image', 'ImageController@index');
+  // simpan image
+  Route::post('image', 'ImageController@store');
+  // hapus image by id
+  Route::delete('image/{id}', 'ImageController@destroy');
 });
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
